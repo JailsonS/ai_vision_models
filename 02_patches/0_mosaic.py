fb73a81ca2510e653c05b0212be4b2d7627b289d
Jailson <engsoaresfilho@gmail.com>
2024-07-24 16:14:56 -0300

    update

--- 02_patches/0_mosaic.py
+++ 02_patches/0_mosaic.py
@@ -1,20 +1,18 @@
-
 import rasterio
-
 from glob import glob
 from rasterio.merge import merge
+from rasterio.plot import show
+from rasterio.transform import from_bounds
+import os
 
 '''
     Config
 '''
 
-
 BASE_PATH = '/home/jailson/Imazon/dl_applications/source/02_patches'
-
-OUTPUT_PATH = f'{BASE_PATH}/data'
-
+OUTPUT_PATH = f'{BASE_PATH}/mosaics'
 YEARS = [
-	#'1985',
+    #'1985',
     #'1986',
     #'1987',
     #'1990',
@@ -28,19 +26,37 @@ YEARS = [
     #'2016'
 ]
 
-
 '''
     Implementation
 '''
 
-    
+
+os.makedirs(OUTPUT_PATH, exist_ok=True)
+
+
 for year in YEARS:
+    
+    input_year_path = f'{BASE_PATH}/data/examples_{year}-*'
+    list_images = [rasterio.open(x) for x in glob(input_year_path)]
+    
+    mosaic, output_transform = merge(list_images, method='first', nodata=0)
 
-	input_year_path = '{}/data/examples_{}-*'.format(BASE_PATH, year)
+    output_path = f'{OUTPUT_PATH}/mosaic_{year}.tif'
 
-	list_images = [rasterio.open(x) for x in glob(input_year_path)]
-	
-	mosaic, output = merge(list_images, method = 'first', nodata = 0)
 
-	print(output)
 
+    out_meta = list_images[0].meta.copy()
+    out_meta.update({
+        "driver": "GTiff",
+        "height": mosaic.shape[1],
+        "width": mosaic.shape[2],
+        "transform": output_transform,
+        "crs": list_images[0].crs
+    })
+
+
+
+    with rasterio.open(output_path, "w", **out_meta) as dest:
+        dest.write(mosaic)
+    
+    print(f'Mosaico para o ano {year} salvo em: {output_path}')
