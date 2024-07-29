4284b84f84a2d647277dd62c0e9e5302d0f56533
Jailson <engsoaresfilho@gmail.com>
2024-07-29 14:20:23 -0300

    update

---
+++ 02_patches/0_mosaic_convert.py
@@ -0,0 +1,18 @@
+from utils.converstion import *
+
+
+
+BASE_PATH = '02_patches/data'
+
+YEARS = [
+    1985
+]
+
+
+for year in YEARS:
+
+    path_geotif = '{}/mosaic_{}.tif'.format(BASE_PATH, str(year))
+    path_netcdf = '{}/netcdf_{}.tif'.format(BASE_PATH, str(year))
+
+    geotiff_to_netcdf(path_geotif, path_netcdf)
+
