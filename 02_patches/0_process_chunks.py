1137c7450310fb28ef16a241498188cfef68a5cb
Jailson <engsoaresfilho@gmail.com>
2024-08-01 12:14:39 -0300

    update

--- 02_patches/0_process_chunks.py
+++ 02_patches/0_process_chunks.py
@@ -1,59 +1,26 @@
 import numpy as np
 from scipy.ndimage import find_objects
 from skimage.measure import label, regionprops
-from collections import defaultdict
 import rasterio
 from glob import glob
 
-
-
+# Definindo o caminho para as imagens e os anos
 PATH_IMAGES = '02_patches/data'
 
 YEARS = [
-    # 
     1985, 1986, 
-    # 1987, 1988, 1989,
-    # 1990, 1991, 1992, 1993, 1994,
-    # 1995, 
-    # 1996, 
-    #1997, 1998, 1999,
-    #2000, 2001, 2002, 2003, 2004,
-    #2005, 2006, 2007, 2008, 2009,
-    #2010, 2011, 2012, 2013, 2014,
-    #2015, 2016, 2017, 2018, 2019,
-    #2020, 2021, 2022
 ]
 
 chunk_size = 600
 
-'''
-    Funcions
-'''
-
-import numpy as np
-from skimage.measure import label
-
-
-
-
+# Funções auxiliares para criar chunks
 def create_chunks(arr, chunk_size):
     chunks = []
     for i in range(0, arr.shape[0], chunk_size):
         for j in range(0, arr.shape[1], chunk_size):
             chunk = arr[i:i + chunk_size, j:j + chunk_size]
-            chunks.append(chunk)
+            chunks.append((chunk, i, j))
     return chunks
-
-def reassemble_chunks(chunks, original_shape, chunk_size):
-    reassembled = np.zeros(original_shape, dtype=int)
-    
-    chunk_idx = 0
-    for i in range(0, original_shape[0], chunk_size):
-        for j in range(0, original_shape[1], chunk_size):
-            reassembled[i:i + chunk_size, j:j + chunk_size] = chunks[chunk_idx]
-            chunk_idx += 1
-    
-    return reassembled
 
 def update_labels(prev_labels, prev_chunk, curr_labels, combined_array):
     if prev_labels is None:
@@ -82,72 +49,56 @@ def update_labels(prev_labels, prev_chunk, curr_labels, combined_array):
     
     return combined_array
 
-
-
-
 previous_labels = None
 
 for idx, year in enumerate(YEARS):
-
     path = f'{PATH_IMAGES}/mosaic_{str(year)}.tif'
 
     array_obj = rasterio.open(path)
 
     proj = {
-        'crs':array_obj.crs,
-        'transform':array_obj.transform
+        'crs': array_obj.crs,
+        'transform': array_obj.transform
     }
 
-    arr = array_obj.read()[0]
+    arr = array_obj.read(1)
     
-    processed_chunks = []
     chunks = create_chunks(arr, chunk_size)
 
-
-
     if idx > 0:
-        arr_prev = rasterio.open(f'{PATH_IMAGES}/mosaic_{str(year-1)}.tif').read()[0]
-        prev_chunks = prev_chunk = create_chunks(arr_prev, chunk_size)
+        arr_prev = rasterio.open(f'{PATH_IMAGES}/mosaic_{str(year-1)}.tif').read(1)
+        prev_chunks = create_chunks(arr_prev, chunk_size)
     else:
-        prev_chunks = [None] * len(chunks)
+        prev_chunks = [(None, None, None)] * len(chunks)
 
-
-    for chunk_idx, chunk in enumerate(chunks):
+    for chunk_idx, (chunk, i, j) in enumerate(chunks):
         combined_array = np.zeros_like(chunk)
         labels = label(chunk, connectivity=1)
         combined_array[:labels.shape[0], :labels.shape[1]] = labels
 
-
         if idx > 0:
-            prev_chunk = prev_chunks[chunk_idx]
+            prev_chunk, prev_i, prev_j = prev_chunks[chunk_idx]
             prev_labels_chunk = previous_labels[chunk_idx] if previous_labels else None
             combined_array = update_labels(prev_labels_chunk, prev_chunk, labels, combined_array)
         
-        processed_chunks.append(combined_array)
+        # Exportando cada chunk processado como arquivo TIFF
+        data = np.expand_dims(combined_array, axis=0)
+        chunk_name = f'{PATH_IMAGES}/chunks/chunk_{str(year)}_{i}_{j}.tif'
 
+        with rasterio.open(
+            chunk_name,
+            'w',
+            driver='GTiff',
+            count=1,
+            height=combined_array.shape[0],
+            width=combined_array.shape[1],
+            dtype=combined_array.dtype,
+            crs=proj['crs'],
+            transform=rasterio.Affine(proj['transform'][0], proj['transform'][1], proj['transform'][2] + j,
+                                      proj['transform'][3], proj['transform'][4], proj['transform'][5] + i)
+        ) as output:
+            output.write(data)
 
-    previous_labels = processed_chunks
-    processed_array = reassemble_chunks(processed_chunks, arr.shape, chunk_size)
+        print(f'Exported {chunk_name} with shape {data.shape}')
 
-
-    # export 
-    data = np.expand_dims(processed_array, axis=0)
-
-    print(f"Classified array at time {str(year)}:\n{processed_array}")
-    
-    name = f'{PATH_IMAGES}/chunks/chunks_{str(year)}.tif'
-
-    with rasterio.open(
-        name,
-        'w',
-        driver = 'COG',
-        count = 1,
-        height = np.array(data).shape[1],
-        width  = np.array(data).shape[2],
-        dtype  = data.dtype,
-        crs    = rasterio.crs.CRS.from_epsg(4326),
-        transform = proj['transform']
-    ) as output:
-        output.write(data)
-
-    print(f'shape {data.shape}')
+    previous_labels = [chunk for chunk, _, _ in chunks]
--- 02_patches/1_combine_chunks.py
+++ 02_patches/1_combine_chunks.py
@@ -1,39 +1,55 @@
 import numpy as np
-import rasterio.dtypes
-import rasterio
-
 from scipy.ndimage import find_objects
-from skimage.measure import label
-from collections import defaultdict
+from skimage.measure import label, regionprops
+import rasterio
 from glob import glob
 from scipy.ndimage import label as label_ndimage
-from pprint import pprint
 
+# Definindo o caminho para as imagens e os anos
 PATH_IMAGES = '02_patches/data'
 
 YEARS = [
-    1985, 1986, 1987, 1988, 1989,
-    1990, 1991, 1992, 1993, 1994, 
-    1996, 
-    1997, 1998, 1999,
-    2000, 2001, 2002, 2003, 2004,
-    2005, 2006, 2007, 2008, 2009,
-    2010, 2011, 2012, 2013, 2014,
-    2015, 2016, 2017, 2018, 2019,
-    2020, 2021, 2022
+    1985, 1986, 
 ]
 
 SET_AREA_LABEL = False
-
 chunk_size = 600
 
-'''
-    Funcions
-'''
+# Funções auxiliares para criar chunks
+def create_chunks(arr, chunk_size):
+    chunks = []
+    for i in range(0, arr.shape[0], chunk_size):
+        for j in range(0, arr.shape[1], chunk_size):
+            chunk = arr[i:i + chunk_size, j:j + chunk_size]
+            chunks.append((chunk, i, j))
+    return chunks
 
-import numpy as np
-from skimage.measure import label
+def update_labels(prev_labels, prev_chunk, curr_labels, combined_array):
+    if prev_labels is None:
+        return combined_array
 
+    props_prev = regionprops(prev_labels)
+    props_curr = regionprops(curr_labels)
+    
+    prev_slices = find_objects(prev_labels)
+    curr_slices = find_objects(curr_labels)
+    
+    label_mapping = {}
+    
+    for prop_curr, curr_slice in zip(props_curr, curr_slices):
+        overlap_labels = set(prev_labels[curr_slice].flatten())
+        overlap_labels.discard(0)
+        
+        if len(overlap_labels) == 1:
+            prev_label = overlap_labels.pop()
+            label_mapping[prop_curr.label] = prev_label
+        else:
+            label_mapping[prop_curr.label] = np.max(prev_labels) + 1
+    
+    for label_value, mapped_label in label_mapping.items():
+        combined_array[curr_labels == label_value] = mapped_label
+    
+    return combined_array
 
 def relabel_array(array, chunk_size=5):
     labeled_array, num_features = label_ndimage(array)
@@ -42,19 +58,18 @@ def relabel_array(array, chunk_size=5):
     for i in range(0, rows, chunk_size):
         for j in range(0, cols, chunk_size):
             if i + chunk_size < rows:
-                update_labels(labeled_array, i + chunk_size - 1, j, i + chunk_size, j)
+                update_labels_local(labeled_array, i + chunk_size - 1, j, i + chunk_size, j)
             if j + chunk_size < cols:
-                update_labels(labeled_array, i, j + chunk_size - 1, i, j + chunk_size)
+                update_labels_local(labeled_array, i, j + chunk_size - 1, i, j + chunk_size)
             if i + chunk_size < rows and j + chunk_size < cols:
-                update_labels(labeled_array, i + chunk_size - 1, j + chunk_size - 1, i + chunk_size, j + chunk_size)
+                update_labels_local(labeled_array, i + chunk_size - 1, j + chunk_size - 1, i + chunk_size, j + chunk_size)
 
     if SET_AREA_LABEL:
         return set_area_label_connected_components(labeled_array)
     else:
         return relabel_connected_components(labeled_array)
-    
 
-def update_labels(array, x1, y1, x2, y2):
+def update_labels_local(array, x1, y1, x2, y2):
     label1 = array[x1, y1]
     label2 = array[x2, y2]
     if label1 != 0 and label2 != 0 and label1 != label2:
@@ -72,7 +87,6 @@ def relabel_connected_components(array):
 
     return relabeled_array
 
-
 def set_area_label_connected_components(array):
     unique_labels = np.unique(array)
     relabeled_array = np.zeros_like(array)
@@ -87,51 +101,33 @@ def set_area_label_connected_components(array):
 
     return relabeled_array
 
+previous_labels = None
 
-
-
-
-
-
-for year in YEARS:
-
-    path = f'{PATH_IMAGES}/chunks/chunks_{str(year)}.tif'
+for idx, year in enumerate(YEARS):
+    path = f'{PATH_IMAGES}/mosaic_{str(year)}.tif'
 
     array_obj = rasterio.open(path)
 
     proj = {
-        'crs':array_obj.crs,
-        'transform':array_obj.transform
+        'crs': array_obj.crs,
+        'transform': array_obj.transform
     }
 
-
-    array = array_obj.read()[0]
-
-    array = relabel_array(array, chunk_size=chunk_size)
-
-    data = np.expand_dims(array, axis=0)
-
-    print(f"Classified array at time {year}:\n{array}")
+    arr = array_obj.read(1)
     
-    name = f'{PATH_IMAGES}/chunks_combined_area_{str(year)}.tif' if SET_AREA_LABEL else f'{PATH_IMAGES}/chunks_combined_{str(year)}.tif'
+    chunks = create_chunks(arr, chunk_size)
 
-    with rasterio.open(
-        name,
-        'w',
-        driver = 'COG',
-        count = 1,
-        height = np.array(data).shape[1],
-        width  = np.array(data).shape[2],
-        dtype  = rasterio.dtypes.float32,
-        crs    = rasterio.crs.CRS.from_epsg(4326),
-        transform = proj['transform']
-    ) as output:
-        output.write(data)
+    if idx > 0:
+        arr_prev = rasterio.open(f'{PATH_IMAGES}/mosaic_{str(year-1)}.tif').read(1)
+        prev_chunks = create_chunks(arr_prev, chunk_size)
+    else:
+        prev_chunks = [(None, None, None)] * len(chunks)
 
-    print(f'shape {data.shape}')
-    
-# rm -r 02_patches/data/examples* 
-# 02_patches/data
-# python3 02_patches/0_mosaic.py
-# vim 02_patches/0_mosaic.py
-# gsutil mv 02_patches/data/mosaics/mosaic_2002.tif gs://imazon/mapbiomas/degradation/fragmentation/forest_mosaic/
+    for chunk_idx, (chunk, i, j) in enumerate(chunks):
+        combined_array = np.zeros_like(chunk)
+        labels = label(chunk, connectivity=1)
+        combined_array[:labels.shape[0], :labels.shape[1]] = labels
+
+        if idx > 0:
+            prev_chunk, prev_i, prev_j = prev_chunks[chunk_idx]
+            prev_labels_chunk = previous_labels
